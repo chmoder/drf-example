c4d8e8b2f66452361290d204f0264ce965190a35
Thomas Cross <tom.bz2@gmail.com>
2022-11-30 14:53:25 -0600

    formatting

--- .terraform/terraform.tf
+++ .terraform/terraform.tf
@@ -1,19 +1,17 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "3.5.0"
     }
   }
   backend "gcs" {
-    bucket  = "chmoder-tfstates"
-    prefix  = "terraform/drf_example"
+    bucket = "chmoder-tfstates"
+    prefix = "terraform/drf_example"
   }
 }
 
 provider "google" {
-#   credentials = file("creds.json")
-
   project = "chmoder"
   region  = "us-central1"
   zone    = "us-central1-c"
