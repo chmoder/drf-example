7e29ae5e0c67b7d1bf2def8f4cf662aea21f0d30
Thomas Cross <tom.bz2@gmail.com>
2022-11-30 15:53:57 -0600

    add iamcredentials api

--- .terraform/apis.tf
+++ .terraform/apis.tf
@@ -20,3 +20,12 @@ resource "google_project_service" "cloudresourcemanager_api" {
     google_project_service.serviceusage_api
   ]
 }
+
+resource "google_project_service" "iamcredentials_api" {
+  project = var.project_id
+  service = "cloudresourcemanager.googleapis.com"
+
+  depends_on = [
+    google_project_service.iamcredentials_api
+  ]
+}
