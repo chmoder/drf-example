557cb7e29148f6a593797dddfc16e3ace492987d
Thomas Cross <tom.bz2@gmail.com>
2022-11-30 15:28:58 -0600

    enable apis for artifact registry

---
+++ .terraform/apis.tf
@@ -0,0 +1,7 @@
+resource "google_project_service" "serviceusage_api" {
+  service = "serviceusage.googleapis.com"
+}
+
+resource "google_project_service" "artifactregistry_api" {
+  service = "artifactregistry.googleapis.com"
+}
