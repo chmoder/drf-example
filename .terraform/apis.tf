cb0b9612871b30b74131ed0a514404bc9af8d237
Thomas Cross <tom.bz2@gmail.com>
2022-11-30 15:31:38 -0600

    depends on logic

--- .terraform/apis.tf
+++ .terraform/apis.tf
@@ -4,4 +4,7 @@ resource "google_project_service" "serviceusage_api" {
 
 resource "google_project_service" "artifactregistry_api" {
   service = "artifactregistry.googleapis.com"
+  depends_on = [
+    google_project_service.serviceusage_api
+  ]
 }
--- .terraform/artifact_registry.tf
+++ .terraform/artifact_registry.tf
@@ -1,6 +1,10 @@
-resource "google_artifact_registry_repository" "my-repo" {
+resource "google_artifact_registry_repository" "chmoder-repo" {
   location      = "us-central1"
   repository_id = "chmoder"
   description   = "chmoder docker repository"
   format        = "DOCKER"
+
+  depends_on = [
+    google_project_service.artifactregistry_api
+  ]
 }
