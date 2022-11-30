4582a3b778444732c808de668e1dc496706709a4
Thomas Cross <tom.bz2@gmail.com>
2022-11-30 15:58:26 -0600

    reorganize

--- .terraform/apis.tf
+++ .terraform/apis.tf
@@ -3,18 +3,18 @@ resource "google_project_service" "serviceusage_api" {
   service = "serviceusage.googleapis.com"
 }
 
-resource "google_project_service" "artifactregistry_api" {
+resource "google_project_service" "cloudresourcemanager_api" {
   project = var.project_id
-  service = "artifactregistry.googleapis.com"
+  service = "cloudresourcemanager.googleapis.com"
 
   depends_on = [
     google_project_service.serviceusage_api
   ]
 }
 
-resource "google_project_service" "cloudresourcemanager_api" {
+resource "google_project_service" "artifactregistry_api" {
   project = var.project_id
-  service = "cloudresourcemanager.googleapis.com"
+  service = "artifactregistry.googleapis.com"
 
   depends_on = [
     google_project_service.serviceusage_api
