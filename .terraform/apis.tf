54ff44a0186e0cb1c1522aff506ff7d44371469a
Thomas Cross <tom.bz2@gmail.com>
2022-11-30 15:40:55 -0600

    use project id in apis

--- .terraform/apis.tf
+++ .terraform/apis.tf
@@ -1,16 +1,21 @@
 resource "google_project_service" "serviceusage_api" {
+  project = var.project_id
   service = "serviceusage.googleapis.com"
 }
 
 resource "google_project_service" "artifactregistry_api" {
+  project = var.project_id
   service = "artifactregistry.googleapis.com"
+
   depends_on = [
     google_project_service.serviceusage_api
   ]
 }
 
 resource "google_project_service" "cloudresourcemanager_api" {
+  project = var.project_id
   service = "cloudresourcemanager.googleapis.com"
+
   depends_on = [
     google_project_service.serviceusage_api
   ]
