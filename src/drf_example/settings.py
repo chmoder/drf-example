a19a233e029159d6c122d065721889939cc7371e
Thomas Cross <tom.bz2@gmail.com>
2022-12-01 11:12:25 -0600

    update settings

--- src/drf_example/settings.py
+++ src/drf_example/settings.py
@@ -32,12 +32,13 @@ DEBUG = env('DEBUG')
 # running in production. The URL will be known once you first deploy
 # to Cloud Run. This code takes the URL and converts it to both these settings formats.
 CLOUDRUN_SERVICE_URL = env("CLOUDRUN_SERVICE_URL", default=None)
+print(CLOUDRUN_SERVICE_URL)
 if CLOUDRUN_SERVICE_URL:
     ALLOWED_HOSTS = [urlparse(CLOUDRUN_SERVICE_URL).netloc]
     CSRF_TRUSTED_ORIGINS = [
         CLOUDRUN_SERVICE_URL, 
         'https://drf-example-6buxidnozq-uc.a.run.app',
-        'https://drf-example.chmoder.org/'
+        'https://drf-example.chmoder.org'
     ]
     SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
