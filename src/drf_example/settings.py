3cb6227e1fdc0a5c94ec664a72da83f623d908dd
Thomas Cross <tom.bz2@gmail.com>
2022-12-01 17:39:41 -0600

    add django cors to settings

--- src/drf_example/settings.py
+++ src/drf_example/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'drf_example.example',
 ]
 
@@ -154,3 +155,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:4200'
+)
