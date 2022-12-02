265a493ccf95ea135f185e36f7d5f177d7dbfebf
Thomas Cross <tom.bz2@gmail.com>
2022-12-02 07:49:58 -0600

    change header priority

--- src/drf_example/settings.py
+++ src/drf_example/settings.py
@@ -61,11 +61,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
@@ -158,8 +158,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:4200'
-# ]
-
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200'
+]
